6218625a855d4275a3870e084630d14feeb0509e
ezrasitorus77 <pratama.ezra7@gmail.com>
2022-03-17 11:36:50 +0700

    major

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ezrasitorus77/go-test
+module github.com/ezrasitorus77/go-test/v2
 
 go 1.17
--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,5 @@
 package gotest
 
-func SayHello() string {
-	return "Hello World!"
+func SayHello(name string) string {
+	return "Hello " + name
 }
